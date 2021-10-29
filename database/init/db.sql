2aba7d92697c779be2ad1c180c3ed694daa2f0bb
finkegabriel <finkegabriel@gmail.com>
2021-10-29 12:10:44 -0700

    update database

--- database/init/db.sql
+++ database/init/db.sql
@@ -6,14 +6,16 @@
 
 CREATE TABLE IF NOT EXISTS barcodes (
   id INTEGER PRIMARY KEY,
+  name TEXT DEFAULT "" NOT NULL,
   code TEXT DEFAULT "" NOT NULL
 );
 
--- INSERT INTO barcodes
---   (code) 
--- VALUES 
---   (""),
---   ("");
+INSERT INTO barcodes
+  (name,code) 
+VALUES 
+  ("Kroger Green Beans","0011110802705"),
+  ("Bush's Best Kidney Beans","0039400017349"),
+  ("Shamrock fat free milk","0028300020306");
 
 CREATE TABLE IF NOT EXISTS list (
   id INTEGER PRIMARY KEY,
